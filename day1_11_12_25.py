d927a2a96f9e9cd08cdf6bba21c2125016e26dc5
Faxriddin <uzcodermelody@gmail.com>
2025-12-11 23:25:37 +0500

    day 2 12/12/2025

--- day1_11_12_25.py
+++ day1_11_12_25.py
@@ -14,9 +14,9 @@
 # print(colors[-1])
 # print(colors[1:3])
 
-# 4
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = a + b
-print(c)
-print(a * 2)
+# # 4
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print(c)
+# print(a * 2)
