46f8b71dffdaf70d511f9fcc6f742154ed29cf59
Faxriddin <uzcodermelody@gmail.com>
2025-12-16 23:53:02 +0500

    dictionary

--- day6.py
+++ day6.py
@@ -29,12 +29,28 @@
 # for key, value in students.items():
 #     print(f"Student {key.title()} {value} years old!")
 
+# students = {
+#     'george': 21,
+#     'Anna': 23,
+#     'lily': 10,
+#     'asad': 33,
+#     'begali': 42
+# }
+# a = [x if x > 18 else "Too young" for x in students.values()]
+# print(a)
+
+# Check if a key exists
+#
+# Check if “Sara” exists in the dictionary.
+#
+# Print a message based on the result.
 students = {
-    'george': 21,
-    'Anna': 23,
-    'lily': 10,
-    'asad': 33,
-    'begali': 42
+    "Ali": 18,
+    "Anna": 20,
+    "John": 19,
+    'sarah':12
 }
-a = [x if x > 18 else "Too young" for x in students.values()]
-print(a)
+
+result = [name for name in students if name.lower() == "sarah"]
+
+print(result[0] if result else "there is no sarah")
