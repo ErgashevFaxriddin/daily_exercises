4fc9bc2f16a76fa0aaa110151d4a5166f44fd7a1
faxriddin ergashev <uzcodermelody@gmail.com>
2026-02-14 23:41:18 +0500

    solved today's exercise

---
+++ functions.py
@@ -0,0 +1,6 @@
+def juft_toq():
+    num = int(input('ENTER A NUMBER: '))
+    if num % 2 == 0:
+        return 'Juft'
+    else:
+        return 'Toq'
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from functions import juft_toq
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+while True:
+    print(juft_toq())
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    choice = input('CONTINUE? (0 / 1)')
+    if choice == '1':
+        continue
+    else:
+        break
+print('FINISHED')
