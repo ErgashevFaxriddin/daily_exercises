4b2e4a7bbf1064030d8e4e2cd0e1d68e56c9eec9
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-03 21:26:01 +0500

    rechecked

--- thirdMarch20260303.py
+++ thirdMarch20260303.py
@@ -4,7 +4,7 @@ numbers = [1, 2, 3, 4, 5, 6]
 
 sum_even = 0
 for num in numbers:
-    if num % 2 == 0:  # check if number is even
+    if num % 2 == 0:
         sum_even += num
 
 print("Sum of even numbers:", sum_even)
