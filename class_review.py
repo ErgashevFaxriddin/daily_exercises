605455d3b5925cfcba4644abe74571b054cc12e7
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-08 23:42:26 +0500

    class review

--- class_review.py
+++ class_review.py
@@ -1,10 +1,23 @@
-class Person:
-    def __init__(self, name):
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def say_hello(self):
+#         print("Hello, my name is", self.name)
+#
+#
+# p1 = Person("Ali")
+# p1.say_hello()
+
+
+class Student:
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
-    def say_hello(self):
-        print("Hello, my name is", self.name)
+    def introduce(self):
+        print("My name is", self.name, "and I am", self.age, "years old.")
 
 
-p1 = Person("Ali")
-p1.say_hello()
+s1 = Student("Sara", 20)
+s1.introduce()
