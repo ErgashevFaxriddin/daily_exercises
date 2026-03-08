a9f30802205d124369fe22f4e159f346424b0f9c
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-08 23:44:35 +0500

    class review

--- class_review.py
+++ class_review.py
@@ -22,15 +22,28 @@
 # s1 = Student("Sara", 20)
 # s1.introduce()
 
+#
+# class Car:
+#     def __init__(self, brand, year):
+#         self.brand = brand
+#         self.year = year
+#
+#     def car_info(self):
+#         print("This car is", self.brand, "made in", self.year)
+#
+#
+# c1 = Car("Toyota", 2020)
+# c1.car_info()
 
-class Car:
-    def __init__(self, brand, year):
-        self.brand = brand
-        self.year = year
 
-    def car_info(self):
-        print("This car is", self.brand, "made in", self.year)
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
 
 
-c1 = Car("Toyota", 2020)
-c1.car_info()
+r1 = Rectangle(5, 4)
+print("Area:", r1.area())
